26f08b0af79f63dabba375fc2a583c92deb2c557
Lidalton S. de Lima <110672018+lidaltonlima@users.noreply.github.com>
2022-08-27 07:27:28 -0400

    add frame

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from msilib.schema import Feature
 import pygame
 from pygame.locals import *
 from sys import exit
@@ -5,16 +6,18 @@ import objects
 
 
 pygame.init()
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((640, 400))
 pygame.display.set_caption('Soroban')
 
-count = objects.Count(screen, (255, 0, 0), (0, 255, 0), 100, 100, 50)
+#count = objects.Count(screen, (255, 0, 0), (0, 255, 0), 100, 100, 50)
+frame = objects.Frame(screen, (255, 0, 0), (0, 255, 0), (0, 0, 255), 600, 250, 10, 10, 10)
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()  
-    count.draw(100, 100)
+            
+    frame.draw(20, 20)
     
     pygame.display.update()
--- objects.py
+++ objects.py
@@ -1,8 +1,9 @@
-from pygame.draw import polygon
+from re import L
+from pygame.draw import polygon, line
 
 class Count:
     def __init__(self, screen, color_top: tuple, color_button: tuple,
-                 width: int, height: int, top_button: int) -> None:
+                 width: int, height: int, top_button: int, value: int = 1) -> None:
         self.screen = screen
         self.color_top = color_top
         self.color_button = color_button
@@ -10,7 +11,7 @@ class Count:
         self.height = height
         self.top_button = top_button
     
-    def draw(self, x, y):
+    def draw(self, x: int, y: int):
         polygon(self.screen, self.color_top, [(x - self.width / 2, y),
                                               (x - self.top_button / 2, y - self.height / 2),
                                               (x + self.top_button / 2, y - self.height / 2),
@@ -19,4 +20,35 @@ class Count:
                                                  (x - self.top_button / 2, y + self.height / 2),
                                                  (x + self.top_button / 2, y + self.height / 2),
                                                  (x + self.width / 2, y)])
-        
+
+
+class Frame:
+    def __init__(self, screen, color_tb: tuple, color_lr: tuple, color_div: tuple, width: int, heigh: int,
+                 weight_tb: int, weight_lr: int, weight_div: int, pos_div: float = 0.4) -> None:
+        self.screen = screen
+        self.color_tb = color_tb
+        self.color_lr = color_lr
+        self.color_div = color_div
+        self.width = width
+        self.height = heigh
+        self.weight_tb = weight_tb
+        self.weight_lr = weight_lr
+        self.weight_div = weight_div
+        self.pos_div = pos_div
+    
+    def draw(self, x: int, y: int):
+        # Top line
+        line(self.screen, self.color_tb, (x, y + self.weight_tb / 2 - 1),
+             (x + self.width, y + self.weight_tb / 2 - 1), width=self.weight_tb)
+        # Button line
+        line(self.screen, self.color_tb, (x, y + self.height - self.weight_tb / 2),
+             (x + self.width, y + self.height - self.weight_tb / 2), width=self.weight_tb)
+        # right line
+        line(self.screen, self.color_lr, (x + self.weight_lr / 2 - 1, y),
+             (x + self.weight_lr / 2 - 1, y + self.height), width=self.weight_lr)
+        # Left line
+        line(self.screen, self.color_lr, (x + self.width - self.weight_lr / 2, y),
+             (x + self.width - self.weight_lr / 2, y + self.height), width=self.weight_lr)
+        # Middle line
+        line(self.screen, self.color_div, (x,self.height * self.pos_div),
+             (x + self.width, self.height * self.pos_div), width=self.weight_div)
